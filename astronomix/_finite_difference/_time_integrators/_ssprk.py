e695760b2a7b668b369298d76f6efd42340d98c1
Leonard Storcks <leonards360@googlemail.com>
2026-02-10 09:02:41 +0100

    cooling now works with the finite difference scheme (still room for improvement), gravity is still experimental

--- astronomix/_finite_difference/_time_integrators/_ssprk.py
+++ astronomix/_finite_difference/_time_integrators/_ssprk.py
@@ -26,10 +26,189 @@ from astronomix._finite_difference._magnetic_update._constrained_transport impor
 )
 from astronomix._finite_difference._maths._differencing import finite_difference_int6
 from astronomix._physics_modules._stellar_wind.stellar_wind import _wind_ei3D_source
+from astronomix._physics_modules.run_physics_modules import _physics_sources
 from astronomix.data_classes.simulation_helper_data import HelperData
 from astronomix.option_classes.simulation_config import SimulationConfig
 from astronomix.option_classes.simulation_params import SimulationParams
 from astronomix.variable_registry.registered_variables import RegisteredVariables
+
+@partial(jax.jit, static_argnames=["registered_variables", "config"], donate_argnames=["conserved_state", "bx_interface", "by_interface", "bz_interface"])
+def _ssprk4_with_ct(
+    conserved_state,
+    bx_interface,
+    by_interface,
+    bz_interface,
+    gamma: Union[float, jnp.ndarray],
+    grid_spacing: Union[float, jnp.ndarray],
+    dt: Union[float, jnp.ndarray],
+    params: SimulationParams,
+    helper_data: HelperData,
+    config: SimulationConfig,
+    registered_variables: RegisteredVariables,
+):
+    """
+    Integrates the MHD equations for one time step using a 5-stage, 4th-order
+    Strong Stability Preserving Runge-Kutta (SSPRK) method
+    with Constrained Transport (CT).
+    """
+
+    # for procceses with similar or smaller time scales as the hydrodynamics,
+    # they should be included as source terms in the RK stages, otherwise
+    # they could be handled outside
+
+    def compute_rhs(current_q, bx, by, bz, k2_coeff):
+        """
+        Computes the right-hand side (RHS) of the MHD equations for a given stage.
+        The `k2_coeff` scales the timestep `dt` for the current RK stage.
+        """
+
+        current_q = update_cell_center_fields(
+            current_q, bx, by, bz, registered_variables
+        )
+
+        dt_tilde = k2_coeff * dt
+
+        # in the future we might support
+        # different grid spacings in each direction
+        dtdx = dt_tilde / grid_spacing
+        dtdy = dt_tilde / grid_spacing
+        dtdz = dt_tilde / grid_spacing
+
+        # Calculate fluxes based on the state of the current stage
+        dF_x = _weno_flux_x(current_q, params.minimum_density, params.minimum_pressure, gamma, config, registered_variables)
+        dF_y = _weno_flux_y(current_q, params.minimum_density, params.minimum_pressure, gamma, config, registered_variables)
+        dF_z = _weno_flux_z(current_q, params.minimum_density, params.minimum_pressure, gamma, config, registered_variables)
+
+        # Calculate RHS for interface magnetic fields using Constrained Transport
+        rhs_bx, rhs_by, rhs_bz = constrained_transport_rhs(
+            current_q, dF_x, dF_y, dF_z, dtdx, dtdy, dtdz, registered_variables
+        )
+
+        # Calculate RHS for conserved fluid variables
+        rhs_q = -dtdx * (
+            (dF_x - jnp.roll(dF_x, 1, axis=1))
+            + (dF_y - jnp.roll(dF_y, 1, axis=2))
+            + (dF_z - jnp.roll(dF_z, 1, axis=3))
+        )
+
+        # Add physics source terms
+        rhs_q += _physics_sources(
+            current_q,
+            dt_tilde,
+            gamma,
+            config,
+            params,
+            helper_data,
+            registered_variables,
+        )
+
+        return rhs_q, rhs_bx, rhs_by, rhs_bz
+
+    # define the SSPRK4 coefficients
+
+    k1_1 = 1.0
+    k2_1 = 0.39175222700392
+    k3_1 = 0.0
+
+    k1_2 = 0.44437049406734
+    k2_2 = 0.36841059262959
+    k3_2 = 0.55562950593266
+
+    k1_3 = 0.62010185138540
+    k2_3 = 0.25189177424738
+    k3_3 = 0.37989814861460
+    
+    k1_4 = 0.17807995410773
+    k2_4 = 0.54497475021237
+    k3_4 = 0.82192004589227
+
+    k1_5 = -2.081261929715610e-02
+    k2_5 = 0.22600748319395
+    k3_5 = 5.03580947213895e-01
+    k4_5 = 0.51723167208978
+    k5_5 = -6.518979800418380e-12
+
+    final_factors = jnp.array([k1_5, 0.0, k4_5, k5_5, k3_5])
+    k_rhs_s = jnp.array([k2_1, k2_2, k2_3, k2_4, k2_5])
+    k_0_s = jnp.array([k1_1, k1_2, k1_3, k1_4, k1_5])
+    k_curr_s = jnp.array([k3_1, k3_2, k3_3, k3_4, k3_5])
+
+    # Store the initial state (t = n)
+    q0 = conserved_state
+    bx0, by0, bz0 = bx_interface, by_interface, bz_interface
+
+    def ssprk_stage(stage_idx, carry):
+
+        # unpack carry
+        current_state, final_state = carry
+        q_curr, bx_curr, by_curr, bz_curr = current_state
+        q_final, bx_final, by_final, bz_final = final_state
+
+        if config.enforce_positivity:
+            q_curr = _enforce_positivity(
+                q_curr,
+                gamma,
+                params.minimum_density,
+                params.minimum_pressure,
+                registered_variables,
+            )
+
+        k_rhs = k_rhs_s[stage_idx]
+        k_0 = k_0_s[stage_idx]
+        k_curr = k_curr_s[stage_idx]
+
+        # update the current state
+        rhs_q, rhs_bx, rhs_by, rhs_bz = compute_rhs(q_curr, bx_curr, by_curr, bz_curr, k_rhs)
+        q_curr = k_0 * q0 + k_curr * q_curr + rhs_q
+        bx_curr = k_0 * bx0 + k_curr * bx_curr + rhs_bx
+        by_curr = k_0 * by0 + k_curr * by_curr + rhs_by
+        bz_curr = k_0 * bz0 + k_curr * bz_curr + rhs_bz
+
+        # update the final state
+        final_factor = final_factors[stage_idx + 1]
+        q_final += q_curr * final_factor
+        bx_final += bx_curr * final_factor
+        by_final += by_curr * final_factor
+        bz_final += bz_curr * final_factor
+
+        return (
+            (q_curr, bx_curr, by_curr, bz_curr), 
+            (q_final, bx_final, by_final, bz_final)
+        )
+
+    (
+        (q4, bx4, by4, bz4),
+        (q_final, bx_final, by_final, bz_final)
+    ) = jax.lax.fori_loop(0, 4, ssprk_stage, 
+        (
+            (q0, bx0, by0, bz0), 
+            (final_factors[0] * q0, final_factors[0] * bx0, final_factors[0] * by0, final_factors[0] * bz0)
+        )
+    )
+
+    # Final Stage (Stage 5)
+    rhs_q4, rhs_bx4, rhs_by4, rhs_bz4 = compute_rhs(q4, bx4, by4, bz4, k2_5)
+    q_final = q_final + rhs_q4
+    bx_final = bx_final + rhs_bx4
+    by_final = by_final + rhs_by4
+    bz_final = bz_final + rhs_bz4
+
+    # Update the cell-centered magnetic fields in the conserved state array
+    # from the final interface magnetic fields.
+    q_final = update_cell_center_fields(
+        q_final, bx_final, by_final, bz_final, registered_variables
+    )
+
+    if config.enforce_positivity:
+        q_final = _enforce_positivity(
+            q_final,
+            gamma,
+            params.minimum_density,
+            params.minimum_pressure,
+            registered_variables,
+        )
+
+    return q_final, bx_final, by_final, bz_final
 
 
 # @partial(jax.jit, static_argnames=["registered_variables", "config"], donate_argnames=["conserved_state", "bx_interface", "by_interface", "bz_interface"])
@@ -173,177 +352,3 @@ from astronomix.variable_registry.registered_variables import RegisteredVariable
 
 #     return q_final, bx_final, by_final, bz_final
 
-
-@partial(jax.jit, static_argnames=["registered_variables", "config"], donate_argnames=["conserved_state", "bx_interface", "by_interface", "bz_interface"])
-def _ssprk4_with_ct(
-    conserved_state,
-    bx_interface,
-    by_interface,
-    bz_interface,
-    gamma: Union[float, jnp.ndarray],
-    grid_spacing: Union[float, jnp.ndarray],
-    dt: Union[float, jnp.ndarray],
-    params: SimulationParams,
-    helper_data: HelperData,
-    config: SimulationConfig,
-    registered_variables: RegisteredVariables,
-):
-    """
-    Integrates the MHD equations for one time step using a 5-stage, 4th-order
-    Strong Stability Preserving Runge-Kutta (SSPRK) method
-    with Constrained Transport (CT).
-    """
-
-    def compute_rhs(current_q, bx, by, bz, k2_coeff):
-        """
-        Computes the right-hand side (RHS) of the MHD equations for a given stage.
-        The `k2_coeff` scales the timestep `dt` for the current RK stage.
-        """
-
-        current_q = update_cell_center_fields(
-            current_q, bx, by, bz, registered_variables
-        )
-
-        # in the future we might support
-        # different grid spacings in each direction
-        dtdx = k2_coeff * dt / grid_spacing
-        dtdy = k2_coeff * dt / grid_spacing
-        dtdz = k2_coeff * dt / grid_spacing
-
-        # Calculate fluxes based on the state of the current stage
-        dF_x = _weno_flux_x(current_q, params.minimum_density, params.minimum_pressure, gamma, config, registered_variables)
-        dF_y = _weno_flux_y(current_q, params.minimum_density, params.minimum_pressure, gamma, config, registered_variables)
-        dF_z = _weno_flux_z(current_q, params.minimum_density, params.minimum_pressure, gamma, config, registered_variables)
-
-        # Calculate RHS for interface magnetic fields using Constrained Transport
-        rhs_bx, rhs_by, rhs_bz = constrained_transport_rhs(
-            current_q, dF_x, dF_y, dF_z, dtdx, dtdy, dtdz, registered_variables
-        )
-
-        # Calculate RHS for conserved fluid variables
-        rhs_q = -dtdx * (
-            (dF_x - jnp.roll(dF_x, 1, axis=1))
-            + (dF_y - jnp.roll(dF_y, 1, axis=2))
-            + (dF_z - jnp.roll(dF_z, 1, axis=3))
-        )
-
-        if config.wind_config.stellar_wind:
-            rhs_q += (
-                _wind_ei3D_source(
-                    params.wind_params,
-                    current_q,
-                    k2_coeff * dt,
-                    config,
-                    helper_data,
-                    config.wind_config.num_injection_cells,
-                    registered_variables,
-                )
-            )
-
-        return rhs_q, rhs_bx, rhs_by, rhs_bz
-
-    # define the SSPRK4 coefficients
-
-    k1_1 = 1.0
-    k2_1 = 0.39175222700392
-    k3_1 = 0.0
-
-    k1_2 = 0.44437049406734
-    k2_2 = 0.36841059262959
-    k3_2 = 0.55562950593266
-
-    k1_3 = 0.62010185138540
-    k2_3 = 0.25189177424738
-    k3_3 = 0.37989814861460
-    
-    k1_4 = 0.17807995410773
-    k2_4 = 0.54497475021237
-    k3_4 = 0.82192004589227
-
-    k1_5 = -2.081261929715610e-02
-    k2_5 = 0.22600748319395
-    k3_5 = 5.03580947213895e-01  # This corresponds to k3 in the Fortran code
-    k4_5 = 0.51723167208978  # This corresponds to k4
-    k5_5 = -6.518979800418380e-12  # This corresponds to k5
-
-    final_factors = jnp.array([k1_5, 0.0, k4_5, k5_5, k3_5])
-    k_rhs_s = jnp.array([k2_1, k2_2, k2_3, k2_4, k2_5])
-    k_0_s = jnp.array([k1_1, k1_2, k1_3, k1_4, k1_5])
-    k_curr_s = jnp.array([k3_1, k3_2, k3_3, k3_4, k3_5])
-
-    # Store the initial state (t = n)
-    q0 = conserved_state
-    bx0, by0, bz0 = bx_interface, by_interface, bz_interface
-
-    def ssprk_stage(stage_idx, carry):
-
-        # unpack carry
-        current_state, final_state = carry
-        q_curr, bx_curr, by_curr, bz_curr = current_state
-        q_final, bx_final, by_final, bz_final = final_state
-
-        if config.enforce_positivity:
-            q_curr = _enforce_positivity(
-                q_curr,
-                gamma,
-                params.minimum_density,
-                params.minimum_pressure,
-                registered_variables,
-            )
-
-        k_rhs = k_rhs_s[stage_idx]
-        k_0 = k_0_s[stage_idx]
-        k_curr = k_curr_s[stage_idx]
-
-        # update the current state
-        rhs_q, rhs_bx, rhs_by, rhs_bz = compute_rhs(q_curr, bx_curr, by_curr, bz_curr, k_rhs)
-        q_curr = k_0 * q0 + k_curr * q_curr + rhs_q
-        bx_curr = k_0 * bx0 + k_curr * bx_curr + rhs_bx
-        by_curr = k_0 * by0 + k_curr * by_curr + rhs_by
-        bz_curr = k_0 * bz0 + k_curr * bz_curr + rhs_bz
-
-        # update the final state
-        final_factor = final_factors[stage_idx + 1]
-        q_final += q_curr * final_factor
-        bx_final += bx_curr * final_factor
-        by_final += by_curr * final_factor
-        bz_final += bz_curr * final_factor
-
-        return (
-            (q_curr, bx_curr, by_curr, bz_curr), 
-            (q_final, bx_final, by_final, bz_final)
-        )
-
-    (
-        (q4, bx4, by4, bz4),
-        (q_final, bx_final, by_final, bz_final)
-    ) = jax.lax.fori_loop(0, 4, ssprk_stage, 
-        (
-            (q0, bx0, by0, bz0), 
-            (final_factors[0] * q0, final_factors[0] * bx0, final_factors[0] * by0, final_factors[0] * bz0)
-        )
-    )
-
-    # Final Stage (Stage 5)
-    rhs_q4, rhs_bx4, rhs_by4, rhs_bz4 = compute_rhs(q4, bx4, by4, bz4, k2_5)
-    q_final = q_final + rhs_q4
-    bx_final = bx_final + rhs_bx4
-    by_final = by_final + rhs_by4
-    bz_final = bz_final + rhs_bz4
-
-    # Update the cell-centered magnetic fields in the conserved state array
-    # from the final interface magnetic fields.
-    q_final = update_cell_center_fields(
-        q_final, bx_final, by_final, bz_final, registered_variables
-    )
-
-    if config.enforce_positivity:
-        q_final = _enforce_positivity(
-            q_final,
-            gamma,
-            params.minimum_density,
-            params.minimum_pressure,
-            registered_variables,
-        )
-
-    return q_final, bx_final, by_final, bz_final
--- astronomix/_physics_modules/run_physics_modules.py
+++ astronomix/_physics_modules/run_physics_modules.py
@@ -6,6 +6,7 @@ import jax.numpy as jnp
 from jaxtyping import Array, Float, jaxtyped
 from beartype import beartype as typechecker
 
+from astronomix._finite_difference._fluid_equations._equations import conserved_state_from_primitive_mhd, primitive_state_from_conserved_mhd
 from astronomix._physics_modules._cnn_mhd_corrector._cnn_mhd_corrector import (
     _cnn_mhd_corrector,
 )
@@ -16,16 +17,19 @@ from astronomix._physics_modules._cosmic_rays.cr_injection import (
 from astronomix._physics_modules._neural_net_force._neural_net_force import (
     _neural_net_force,
 )
+from astronomix._physics_modules._self_gravity._poisson_solver import _compute_gravitational_potential
+from astronomix._stencil_operations._stencil_operations import _stencil_add
 from astronomix.data_classes.simulation_helper_data import HelperData
 from astronomix._geometry.boundaries import _boundary_handler
 from astronomix.variable_registry.registered_variables import RegisteredVariables
 from astronomix.option_classes.simulation_config import (
+    SIMPLE_SOURCE_TERM,
     SPHERICAL,
     STATE_TYPE,
     SimulationConfig,
 )
 from astronomix.option_classes.simulation_params import SimulationParams
-from astronomix._physics_modules._stellar_wind.stellar_wind import _wind_injection
+from astronomix._physics_modules._stellar_wind.stellar_wind import _wind_ei3D_source, _wind_injection
 from astronomix.shock_finder.shock_finder import shock_criteria
 
 
@@ -126,3 +130,103 @@ def _run_physics_modules(
         )
 
     return primitive_state
+
+# for now we add a function here which handles
+# physics modules in terms of a source term
+# for the SSPRK time integrator in the WENO
+# scheme, TODO: streamline FD and FV codes
+
+@partial(jax.jit, static_argnames=["config", "registered_variables"])
+def _physics_sources(
+    conserved_state: STATE_TYPE,
+    dt: Float[Array, ""],
+    gamma: Union[float, Float[Array, ""]],
+    config: SimulationConfig,
+    params: SimulationParams,
+    helper_data: HelperData,
+    registered_variables: RegisteredVariables,
+) -> STATE_TYPE:
+    """
+    Compute the physics source terms for the given **conserved** state.
+
+    Args:
+        conserved_state: The conserved state array.
+        gamma: The adiabatic index.
+        config: The simulation configuration.
+        params: The simulation parameters.
+        helper_data: The helper data.
+        registered_variables: The registered variables.
+    Returns:
+        The physics source terms for the conserved state.
+    """
+
+    S = jnp.zeros_like(conserved_state)
+
+    if config.wind_config.stellar_wind:
+        S += (
+            _wind_ei3D_source(
+                params.wind_params,
+                conserved_state,
+                dt,
+                config,
+                helper_data,
+                config.wind_config.num_injection_cells,
+                registered_variables,
+            )
+        )
+
+    if config.cooling_config.cooling:
+        primitive_state = primitive_state_from_conserved_mhd(
+            conserved_state,
+            params.minimum_density,
+            params.minimum_pressure,
+            gamma,
+            config,
+            registered_variables
+        )
+        primitive_state = update_pressure_by_cooling(
+            primitive_state,
+            registered_variables,
+            config.cooling_config,
+            params,
+            dt,
+        )
+        S += (conserved_state_from_primitive_mhd(
+            primitive_state, gamma, registered_variables
+        ) - conserved_state)
+
+    # simplest self-gravity
+    # TODO: maybe only one Poisson solve per RK step?
+    if config.self_gravity:
+        if config.self_gravity_version == SIMPLE_SOURCE_TERM:
+            gravitational_potential = _compute_gravitational_potential(
+                conserved_state[registered_variables.density_index],
+                config.grid_spacing,
+                config,
+                params.gravitational_constant
+            )
+            for axis in range(1, config.num_dimensions + 1):
+                rho = primitive_state[registered_variables.density_index]
+                v_axis = primitive_state[axis]
+
+                # TODO: use higher-order finite difference
+                # a_i = - (phi_{i+1} - phi_{i-1}) / (2 * dx)
+                acceleration = -_stencil_add(
+                    gravitational_potential, indices=(1, -1), factors=(1.0, -1.0), axis=axis - 1
+                ) / (2 * config.grid_spacing)
+                # it is axis - 1 because the axis is 1-indexed as usually the zeroth axis are the different
+                # fields in the state vector not the spatial dimensions, but here we only have the spatial dimensions
+
+                S_axis = jnp.zeros_like(primitive_state)
+                S_axis = S_axis.at[axis].set(rho * acceleration)
+                S_axis = S_axis.at[registered_variables.pressure_index].set(
+                    rho * v_axis * acceleration
+                )
+
+                S += S_axis
+        else:
+            raise NotImplementedError(
+                "Only SIMPLE_SOURCE_TERM self-gravity is implemented for the finite difference scheme."
+            )
+    
+    return S
